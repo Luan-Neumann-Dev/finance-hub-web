05f866f616637ba6aab76a1d4b44097ee0bef250
Luan Henrique Neumann <luan.neumann.dev@gmail.com>
2026-03-27 10:44:41 -0300

    feat: create textarea component

---
+++ src/components/ui/textarea.tsx
@@ -0,0 +1,24 @@
+import { cn } from "@/lib/utils";
+import * as React from "react";
+
+export type TextareaProps = React.TextareaHTMLAttributes<HTMLTextAreaElement>;
+
+const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(
+  ({ className, ...props }, ref) => (
+    <textarea
+      ref={ref}
+      className={cn(
+        "flex min-h-20 w-full rounded-xl border border-input bg-background px-3 py-2 text-sm",
+        "placeholder:text-muted-foreground",
+        "focus:outline-none focus:ring-2 focus:ring-ring",
+        "disabled:cursor-not-allowed disabled:opacity-50",
+        "resize-none transition-all",
+        className
+      )}
+      {...props}
+    />
+  )
+)
+Textarea.displayName = "Textarea";
+
+export { Textarea}
