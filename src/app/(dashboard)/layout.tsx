94b162a940e5022f65932c5e672b29f8040fba4c
Luan Neumann Dev <luan.neumann.dev@gmail.com>
2026-03-29 16:46:12 -0300

    chore: update dashboard layout

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -6,11 +6,11 @@ export default function DashboardLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="flex h-screen bg-background">
+    <div className="min-h-screen bg-background">
       <Sidebar />
-
-      <main className="lg:pl-64">
-        <div className="min-h-screen p-4 sm:p-6 lg:p-8 pt-16 lg:pt-8">
+      
+      <main className="lg:pl-64 min-h-screen">
+        <div className="p-4 sm:p-6 sm:pt-16 lg:p-8 pt-16 lg:pt-8">
           {children}
         </div>
       </main>
